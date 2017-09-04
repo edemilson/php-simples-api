881e826b9a773d83b2f89567256a80a3c7304c07
Edemilson <edemilson@fulltime.com.br>
2017-09-04 16:57:17 -0300

    adicionado cliente como exemplo

---
+++ application/Controller/Cliente.php
@@ -0,0 +1,9 @@
+<?php
+
+    namespace Controller;
+    use Root\Controller;
+
+    class Cliente extends Controller{
+
+
+    }
---
+++ application/Model/Cliente.php
@@ -0,0 +1,8 @@
+<?php
+
+    namespace Model;
+    use Root\Model;
+
+    class Cliente extends Model{
+
+    }
